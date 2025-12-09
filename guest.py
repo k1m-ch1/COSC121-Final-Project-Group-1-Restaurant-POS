2d2f551168f6bee8f285b6307d34d0c5b6d03f57
k1m-ch1 <jingluy123@gmail.com>
2025-12-09 19:51:00 +0700

    made selection interface a bit better

--- guest.py
+++ guest.py
@@ -55,10 +55,11 @@ def select_item(menu_list:list) -> str|None:
     """
     # might want a more descriptive error handling
     # refactor later...
-    selected_item = iterfzf([f"{Fore.GREEN}{menu_item['category']:{max(map(lambda menu_item: len(menu_item['category']), menu_list))}}{Style.RESET_ALL}: {menu_item['name']:20} ${str(menu_item['price']):5} ({str(menu_item['id'])})" for menu_item in menu_list], prompt="Select an item: ", ansi=True)
-    if selected_item == None:
+    try:
+        selected_item = iterfzf([f"{Fore.GREEN}{menu_item['category']:{max(map(lambda menu_item: len(menu_item['category']), menu_list))}}{Style.RESET_ALL}: {menu_item['name']:20} ${str(menu_item['price']):5} ({str(menu_item['id'])})" for menu_item in menu_list], prompt="Select an item: ", ansi=True)
+        return re.match(r".*[(](.+)[)].*", selected_item).group(1)
+    except:
         return None
-    return re.match(r".*[(](.+)[)].*", selected_item).group(1)
 
 def load_all_orders(all_orders_file_path:str) -> dict:
     """Returns an 'all_orders' dictionary by reading an all orders json file"""
